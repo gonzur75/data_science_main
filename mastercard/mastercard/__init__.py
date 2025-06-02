3e685e1705e8f470b9f1ec27520a6b692aadd543
Marcin <gonzur75@gmail.com>
2025-06-02 11:30:58 +0200

    noetes;

--- mastercard/mastercard/__init__.py
+++ mastercard/mastercard/__init__.py
@@ -1,1 +1,3 @@
 from mastercard import config  # noqa: F401
+from mastercard.visualisation.numerical_data import dist_visualisation
+from mastercard.visualisation.categorical_data import dist_categorical_visualisation
